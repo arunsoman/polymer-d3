d759e50f0fdc56c0bb5ba70ea2c36941d807e861
sravan <sravan.s@flytxt.com>
2016-08-02 15:47:25 +0530

    Created inputs behavior. Bound dropdowns to inputs.

--- namespace.js
+++ namespace.js
@@ -7,4 +7,28 @@ PolymerD3.utilities.merge = function(from, to) {
   this[to].forEach(function (t) {
     me.push(from, t);
   });
-}
+};
+
+PolymerD3.utilities._setProperty = function(arr, key) {
+  if(arguments.length < 1){
+      return;
+  }
+  newVal = arguments[1];
+  this[arr].forEach(function(elem){
+      if (elem.input === key){
+          for(var newKey in newVal){
+              elem[newKey] = newVal[newKey];
+          }
+      }
+  });
+};
+
+PolymerD3.utilities._getProperty = function(arr, key) {
+  var result;
+  this[arr].forEach(function(elem) {
+      if(elem.input === key){
+          result = elem;
+      }
+  });
+  return result;
+};
