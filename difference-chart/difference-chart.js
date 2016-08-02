56c64c1156d0cd509acb7cd19263e5248b1aa4b0
ess_ess <sravans2011@gmail.com>
2016-08-02 20:28:14 +0530

    Fixed diffrence chart

--- difference-chart/difference-chart.js
+++ difference-chart/difference-chart.js
@@ -98,7 +98,7 @@ Polymer({
                 return y(d[y1Name]);
             });
 
-        this.svg 
+        me.svg
             .attr("width", width + margin.left + margin.right)
             .attr("height", height + margin.top + margin.bottom)
             .append("g")
@@ -126,40 +126,40 @@ Polymer({
                 })
             ]);
 
-            this.svg.datum(data);
+            me.svg.datum(data);
 
-            this.svg.append("clipPath")
+            me.svg.append("clipPath")
                 .attr("id", "clip-below")
                 .append("path")
                 .attr("d", area.y0(height));
 
-            svg.append("clipPath")
+            me.svg.append("clipPath")
                 .attr("id", "clip-above")
                 .append("path")
                 .attr("d", area.y0(0));
 
-            svg.append("path")
+            me.svg.append("path")
                 .attr("class", "area above")
                 .attr("clip-path", "url(#clip-above)")
                 .attr("d", area.y0(function(d) {
                     return y(d[y2Name]);
                 }));
 
-            svg.append("path")
+            me.svg.append("path")
                 .attr("class", "area below")
                 .attr("clip-path", "url(#clip-below)")
                 .attr("d", area);
 
-            svg.append("path")
+            me.svg.append("path")
                 .attr("class", "line")
                 .attr("d", line);
 
-            svg.append("g")
+            me.svg.append("g")
                 .attr("class", "x axis")
                 .attr("transform", "translate(0," + height + ")")
                 .call(xAxis);
 
-            svg.append("g")
+            me.svg.append("g")
                 .attr("class", "y axis")
                 .call(yAxis)
                 .append("text")
