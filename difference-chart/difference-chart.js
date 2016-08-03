fe10c0078f0d97f865aafc1aaeede7d511dc8c30
shiny <shiny.raj@flytxt.com>
2016-08-03 16:32:42 +0530

    Rewrite draw function according to new input

--- difference-chart/difference-chart.js
+++ difference-chart/difference-chart.js
@@ -33,6 +33,11 @@ Polymer({
             type: Array,
             value: []
         },
+        source: {
+          type: Array,
+          value: [],
+          notify: true
+        },
         hideSettings: true,
         data: String,
         external: Array
@@ -104,13 +109,12 @@ Polymer({
             .append("g")
             .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-        d3.tsv("data.tsv", function(error, data) {
-            if (error) throw error;
-
-            data.forEach(function(d) {
-                d[xName] = parseDate(d[xName]);
-                d[y1Name] = +d[y1Name];
-                d[y2Name] = +d[y2Name];
+            var data = me.source;
+            if (data) {
+               data.forEach(function(d) {
+                d[xName] = parseDate(d[0]);
+                d[y1Name] = +d[1];
+                d[y2Name] = +d[2];
             });
 
             x.domain(d3.extent(data, function(d) {
@@ -119,10 +123,10 @@ Polymer({
 
             y.domain([
                 d3.min(data, function(d) {
-                    return Math.min(d[y1Name], d[y2Name]);
+                    return Math.min(d[1], d[2]);
                 }),
                 d3.max(data, function(d) {
-                    return Math.max(d[y1Name], d[y2Name]);
+                    return Math.max(d[1], d[2]);
                 })
             ]);
 
@@ -142,7 +146,7 @@ Polymer({
                 .attr("class", "area above")
                 .attr("clip-path", "url(#clip-above)")
                 .attr("d", area.y0(function(d) {
-                    return y(d[y2Name]);
+                    return y(d[2]);
                 }));
 
             me.svg.append("path")
@@ -167,8 +171,73 @@ Polymer({
                 .attr("y", 6)
                 .attr("dy", ".71em")
                 .style("text-anchor", "end")
-                .text("Temperature (ºF)");
-        });
+                .text("Temperature (ºF)"); 
+            }
+        // d3.tsv("data.tsv", function(error, data) {
+        //     if (error) throw error;
+
+        //     data.forEach(function(d) {
+        //         d[xName] = parseDate(d[xName]);
+        //         d[y1Name] = +d[y1Name];
+        //         d[y2Name] = +d[y2Name];
+        //     });
+
+        //     x.domain(d3.extent(data, function(d) {
+        //         return d[xName];
+        //     }));
+
+        //     y.domain([
+        //         d3.min(data, function(d) {
+        //             return Math.min(d[y1Name], d[y2Name]);
+        //         }),
+        //         d3.max(data, function(d) {
+        //             return Math.max(d[y1Name], d[y2Name]);
+        //         })
+        //     ]);
+
+        //     me.svg.datum(data);
+
+        //     me.svg.append("clipPath")
+        //         .attr("id", "clip-below")
+        //         .append("path")
+        //         .attr("d", area.y0(height));
+
+        //     me.svg.append("clipPath")
+        //         .attr("id", "clip-above")
+        //         .append("path")
+        //         .attr("d", area.y0(0));
+
+        //     me.svg.append("path")
+        //         .attr("class", "area above")
+        //         .attr("clip-path", "url(#clip-above)")
+        //         .attr("d", area.y0(function(d) {
+        //             return y(d[y2Name]);
+        //         }));
+
+        //     me.svg.append("path")
+        //         .attr("class", "area below")
+        //         .attr("clip-path", "url(#clip-below)")
+        //         .attr("d", area);
+
+        //     me.svg.append("path")
+        //         .attr("class", "line")
+        //         .attr("d", line);
+
+        //     me.svg.append("g")
+        //         .attr("class", "x axis")
+        //         .attr("transform", "translate(0," + height + ")")
+        //         .call(xAxis);
+
+        //     me.svg.append("g")
+        //         .attr("class", "y axis")
+        //         .call(yAxis)
+        //         .append("text")
+        //         .attr("transform", "rotate(-90)")
+        //         .attr("y", 6)
+        //         .attr("dy", ".71em")
+        //         .style("text-anchor", "end")
+        //         .text("Temperature (ºF)");
+        // });
 
     }
 
