c207db52b6094f4b02c752036fbd5bf27d99d82f
sravan <sravan.s@flytxt.com>
2016-08-04 10:57:11 +0530

    Basic stacked bar chart implemented. Issues to fix: Place axis at left(Or may be implement an 'axis settings' behavior) Implement grouped barchart logic into this.

--- bar-chart/bar-chart.js
+++ bar-chart/bar-chart.js
@@ -14,7 +14,7 @@ Polymer({
       }, {
         input: 'y',
         txt: 'Pick measures',
-        selectedValue: [1, 2],
+        selectedValue: [2, 1],
         selectedName: [],
         uitype: 'multi-value'
       }, {
@@ -59,7 +59,7 @@ Polymer({
       .attr('class', 'd3-tip')
       .offset([-10, 0])
       .html(function(d) {
-        return "<strong>Frequency:</strong> <span style='color:red'>" + "</span>";
+        return '<strong>Frequency:</strong> <span style=\'color:red\'>' + '</span>';
       });
     this.svg.call(this.tip);
     this.svg.selectAll('.layer')
@@ -68,196 +68,65 @@ Polymer({
       .on('mouseout', this.tip.hide);
   },
 
-  /*draw: function() {
-      var me = this;
-      var n = this.getInputsProperty('y').length; // number of layers
-      var m = this.source.length; // number of samples per layer
-      var stack = d3.layout.stack();
-      var layers = stack(me.source);
-      var yGroupMax = d3.max(layers, function(array) {
-        return d3.max(array.filter(function(value) {
-          return typeof value === "number";
-        }));
-      });
-      var yStackMax = d3.max(layers, function(layer) {
-          var sum = 0;
-          var elems = me.getInputsProperty('y');
-          elems.forEach(function(array) {
-              sum += layer[array];
-          });
-          layer.push(sum);
-          return sum;
-
-      });
-
-      var margin = this.getMargins();
-      var width = this.getWidth() - margin.left - margin.right;
-      var height = this.getHeight() - margin.top - margin.bottom;
-
-      var x = d3.scale.ordinal()
-          .domain(d3.range(m))
-          .rangeRoundBands([0, width], .08);
-
-      var y = d3.scale.linear()
-          .domain([0, yStackMax])
-          .range([height, 0]);
-
-      var color = d3.scale.linear()
-          .domain([0, n - 1])
-          .range([this.getSettingsPropertyObj('colorRange').to, this.getSettingsPropertyObj('colorRange').from]);
-
-      var xAxis = d3.svg.axis()
-          .scale(x)
-          .tickSize(0)
-          .tickPadding(6)
-          .orient("bottom");
-
-      this.makeChartWrap();
-
-      this.svg.attr("width", width + margin.left + margin.right)
-          .attr("height", height + margin.top + margin.bottom)
-          .append("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      var layer = this.svg.selectAll(".layer")
-          .data(layers)
-          .enter().append("g")
-          .attr("class", "layer")
-          .style("fill", function(d, i) {
-              return color(i);
-          });
-
-      var rect = layer.selectAll("rect")
-          .data(function(d) {
-              console.log(me, 'barchart');
-              return d;
-          })
-          .enter().append("rect")
-          .attr("x", function(d) {
-              return x(d[me.getInputsProperty('x')]);
-          })
-          .attr("y", height)
-          .attr("width", x.rangeBand())
-          .attr("height", 0);
-
-      rect.transition()
-          .delay(function(d, i) {
-              return i * 10;
-          })
-          .attr("y", function(d) {
-              var elems = me.getInputsProperty('y');
-              var sum = 0;
-              elems.forEach(function(e) {
-                  sum += d[e];
-              });
-              return y(sum);
-          })
-          .attr("height", function(d) {
-              if(y(d[me.getInputsProperty('y')[0]]) - y(d[d.length - 1]) < 0 ){
-                  throw new Error("height for chart can't be negative", 'bar-chart.rect.transition(');
-              }
-              return y(d[me.getInputsProperty('y')[0]]) - y(d[d.length - 1]);
-          });
-      this.svg.append("g")
-          .attr("class", "x axis")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-      function transitionGrouped() {
-          y.domain([0, yGroupMax]);
-
-          rect.transition()
-              .duration(500)
-              .delay(function(d, i) {
-                  return i * 10;
-              })
-              .attr("x", function(d, i, j) {
-                  return x(d[me.getInputsProperty('x')]) + x.rangeBand() / n * j;
-              })
-              .attr("width", x.rangeBand() / n)
-              .transition()
-              .attr("y", function(d) {
-                  return y(d[me.getInputsProperty('y')[0]]);
-              })
-              .attr("height", function(d) {
-                  if(y(d[me.getInputsProperty('y')[0]]) - y(d[d.length - 1]) < 0){
-                      throw new Error("height for chart can't be negative", 'bar-chart.transitionGrouped');
-                  }
-                  return height - y(d[me.getInputsProperty('y')[0]]);
-              });
-      };
-
-      function transitionStacked() {
-          y.domain([0, yStackMax]);
-
-          rect.transition()
-              .duration(500)
-              .delay(function(d, i) {
-                  return i * 10;
-              })
-              .attr("y", function(d) {
-                  return y(d[d.length - 1]);
-              })
-              .attr("height", function(d) {
-                  if(y(d[me.getInputsProperty('y')[0]]) - y(d[d.length - 1]) < 0){
-                      throw new Error("height for chart can't be negative", 'bar-chart.transitionStacked');
-                  }
-                  return y(d[me.getInputsProperty('y')[0]]) - y(d[d.length - 1]);
-              })
-              .transition()
-              .attr("x", function(d) {
-                  return x(d[me.getInputsProperty('x')]);
-              })
-              .attr("width", x.rangeBand());
-      };
-      this._addToolTip();
-      return [transitionStacked, transitionGrouped];
-
-  },*/
-
   draw: function() {
-    // var causes = ["wounds", "other", "disease"];
 
-    var parseDate = d3.time.format("%m/%Y").parse;
+    var parseDate = d3.time.format('%m/%Y').parse;
     var me = this;
     var margin = this.getMargins();
     var width = this.getWidth() - margin.left - margin.right;
     var height = this.getHeight() - margin.top - margin.bottom;
 
+    // Selects stack of elements as Y-Axis
+    var selected = this.getInputsProperty('y');
+
+    // X axis
     var x = d3.scale.ordinal()
       .rangeRoundBands([0, width]);
 
+    // Y Axis
     var y = d3.scale.linear()
       .rangeRound([height, 0]);
 
+    // Colors
     var z = d3.scale.category10();
 
+    //Set X Axis at Bottom
     var xAxis = d3.svg.axis()
       .scale(x)
-      .orient("bottom");
+      .orient('bottom');
 
+    // Sets Y axis at right
     var yAxis = d3.svg.axis()
       .scale(y)
-      .orient("right");
+      .orient('left');
 
+    // Parent SVG
     var svg = this.svg
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom)
-      .append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+      .attr('width', width + margin.left + margin.right)
+      .attr('height', height + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
+    // Sets data source
     var src = me.source;
 
+    if (!src) {
+      console.warn('Data source empty');
+      return false;
+    }
 
-    var layers = d3.layout.stack()(causes.map(function(c) {
+    // Create layers based on stack
+    // Parses the data as : {x: '',y: '',y0: ''}
+    var layers = d3.layout.stack()(selected.map(function(c) {
       return src.map(function(d) {
         return {
-          x: d[0],
-          y: d[1]
+          x: d[me.getInputsProperty('x')],
+          y: d[c]
         };
       });
     }));
 
+    //Draws the chart with newly mapped data
     x.domain(layers[0].map(function(d) {
       return d.x;
     }));
@@ -266,40 +135,38 @@ Polymer({
       return d.y0 + d.y;
     })]).nice();
 
-    var layer = svg.selectAll(".layer")
+    var layer = svg.selectAll('.layer')
       .data(layers)
-      .enter().append("g")
-      .attr("class", "layer")
-      .style("fill", function(d, i) {
+      .enter().append('g')
+      .attr('class', 'layer')
+      .style('fill', function(d, i) {
         return z(i);
       });
 
-    layer.selectAll("rect")
+    layer.selectAll('rect')
       .data(function(d) {
         return d;
       })
-      .enter().append("rect")
-      .attr("x", function(d) {
+      .enter().append('rect')
+      .attr('x', function(d) {
         return x(d.x);
       })
-      .attr("y", function(d) {
+      .attr('y', function(d) {
         return y(d.y + d.y0);
       })
-      .attr("height", function(d) {
+      .attr('height', function(d) {
         return y(d.y0) - y(d.y + d.y0);
       })
-      .attr("width", x.rangeBand() - 1);
+      .attr('width', x.rangeBand() - 1);
 
-    svg.append("g")
-      .attr("class", "axis axis--x")
-      .attr("transform", "translate(0," + height + ")")
+    svg.append('g')
+      .attr('class', 'axis axis--x')
+      .attr('transform', 'translate(0,' + height + ')')
       .call(xAxis);
 
-    svg.append("g")
-      .attr("class", "axis axis--y")
-      .attr("transform", "translate(" + width + ",0)")
+    svg.append('g')
+      .attr('class', 'axis axis--y')
+      .attr('transform', 'translate(' + width + ',0)')
       .call(yAxis);
-
-
   }
 });
