f6dd8ed3bf22172996e6a56089e8742e6bac0b2f
aarunsoman <arunsoman@gmail.com>
2016-08-13 01:15:44 +0530

    difference-chart with new API

--- difference-chart/difference-chart.js
+++ difference-chart/difference-chart.js
@@ -8,32 +8,32 @@ Polymer({
             value: [{
                 input: 'x',
                 txt: 'Pick a dimension',
-                selectedValue: 0,
+                selectedValue: -1,
                 selectedName: 'date',
                 uitype: 'single-value',
                 selectedObjs: [],
                 scaleType: '',
-                format:'',
+                format: '',
                 maxSelectableValues: 1
             }, {
                 input: 'y',
                 txt: 'Pick measure 1',
-                selectedValue: 0,
+                selectedValue: -1,
                 selectedName: 'New York',
                 uitype: 'single-value',
                 selectedObjs: [],
                 scaleType: '',
-                format:'',
+                format: '',
                 maxSelectableValues: 1
             }, {
                 input: 'z',
                 txt: 'Pick measure 2',
-                selectedValue: 0,
+                selectedValue: -1,
                 selectedName: 'San Francisco',
                 uitype: 'single-value',
                 selectedObjs: [],
                 scaleType: '',
-                format:'',
+                format: '',
                 maxSelectableValues: 1
             }]
         },
@@ -43,9 +43,9 @@ Polymer({
             value: []
         },
         source: {
-          type: Array,
-          value: [],
-          notify: true
+            type: Array,
+            value: [],
+            notify: true
         },
         hideSettings: true,
         data: String,
@@ -60,187 +60,122 @@ Polymer({
         this.hideSettings = !this.hideSettings;
         // this.chart = this.draw();
     },
+    attached: function() {
+        me = this;
+
+        function callme(data) {
+            me.source = data;
+        }
+        PolymerD3.fileReader('data-diff-chart.csv', [1, 2], [0], "%Y%m%d", callme);
+        // this.imputs[0].selectedValue = 0;
+        // this.imputs[1].selectedValue = 1;
+        // this.imputs[2].selectedValue = 2;
+    },
 
     draw: function() {
         var me = this;
-        var xName = this.inputs[0].selectedName;
-        var y1Name = this.inputs[1].selectedName;
-        var y2Name = this.inputs[2].selectedName;
+        var x0 = 0; //this.inputs[0].selectedValue;
+        var y0 = 1; //this.inputs[1].selectedValue;
+        var y1 = 2; //this.inputs[2].selectedValue;
 
-        var margin = this.getMargins();
-        var width = this.getWidth() - margin.left - margin.right;
-        var height = this.getHeight() - margin.top - margin.bottom;
 
-        var parseDate = d3.time.format("%Y%m%d").parse;
+        var dataSummary = d3.nest().rollup((k) => {
+            //console.log(d);
+            return {
+                'xBound': d3.extent(k, (d) => {
+                    return d[x0]
+                }),
+                'yMin': d3.min(k, (d) => {
+                    return Math.min(d[y0], d[y1])
+                }),
+                'yMax': d3.max(k, (d) => {
+                    return Math.max(d[y0], d[y1])
+                })
+            }
+        }).entries(me.source);
 
-        var x = d3.time.scale()
-            .range([0, width]);
+        var config = {
+            'scaleType': "time",
+            'align': 'h',
+            'format': 'time',
+            'position': 'bottom',
+            'domain': dataSummary.xBound
+        };
+        var xAxis = me.createAxis(config);
+        var x = xAxis.scale();
 
-        var y = d3.scale.linear()
-            .range([height, 0]);
-
-        var xAxis = d3.svg.axis()
-            .scale(x)
-            .orient("bottom");
-
-        var yAxis = d3.svg.axis()
-            .scale(y)
-            .orient("left");
+        config = {
+            'scaleType': "linear",
+            'align': 'v',
+            'format': 'currency',
+            'position': 'left',
+            'domain': [0, dataSummary.yMax]
+        };
+        var yAxis = me.createAxis(config);
+        var y = yAxis.scale();
 
         var line = d3.svg.area()
             .interpolate("basis")
-            .x(function(d) {
-                return x(d[xName]);
+            .x((d) => {
+                return x(d[x0]);
             })
-            .y(function(d) {
-                return y(d[y1Name]);
+            .y((d) => {
+                return y(d[y1]);
             });
 
         var area = d3.svg.area()
             .interpolate("basis")
-            .x(function(d) {
-                return x(d[xName]);
+            .x((d) => {
+                return x(d[x0]);
             })
-            .y1(function(d) {
-                return y(d[y1Name]);
+            .y1((d) => {
+                return y(d[y1]);
             });
 
-        me.svg
-            .attr("width", width + margin.left + margin.right)
-            .attr("height", height + margin.top + margin.bottom)
-            .append("g")
-            .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+        var cc = me.parentG.append('g');
+        cc.datum(me.source);
 
-            var data = me.source;
-            if (data) {
-               data.forEach(function(d) {
-                d[xName] = parseDate(d[0]);
-                d[y1Name] = +d[1];
-                d[y2Name] = +d[2];
-            });
+        cc.append("clipPath")
+            .attr("id", "clip-below")
+            .append("path")
+            .attr("d", area.y0(this.chartHeight));
 
-            x.domain(d3.extent(data, function(d) {
-                return d[xName];
+        cc.append("clipPath")
+            .attr("id", "clip-above")
+            .append("path")
+            .attr("d", area.y0(0));
+
+        cc.append("path")
+            .attr("class", "area above")
+            .attr("clip-path", "url(#clip-above)")
+            .attr("d", area.y0(function(d) {
+                return y(d[2]);
             }));
 
-            y.domain([
-                d3.min(data, function(d) {
-                    return Math.min(d[1], d[2]);
-                }),
-                d3.max(data, function(d) {
-                    return Math.max(d[1], d[2]);
-                })
-            ]);
+        cc.append("path")
+            .attr("class", "area below")
+            .attr("clip-path", "url(#clip-below)")
+            .attr("d", area);
 
-            me.svg.datum(data);
+        cc.append("path")
+            .attr("class", "line")
+            .attr("d", line);
+        /*
+                    me.parentG.append("g")
+                        .attr("class", "x axis")
+                        .attr("transform", "translate(0," + this.chartHeight + ")")
+                        .call(xAxis);
 
-            me.svg.append("clipPath")
-                .attr("id", "clip-below")
-                .append("path")
-                .attr("d", area.y0(height));
-
-            me.svg.append("clipPath")
-                .attr("id", "clip-above")
-                .append("path")
-                .attr("d", area.y0(0));
-
-            me.svg.append("path")
-                .attr("class", "area above")
-                .attr("clip-path", "url(#clip-above)")
-                .attr("d", area.y0(function(d) {
-                    return y(d[2]);
-                }));
-
-            me.svg.append("path")
-                .attr("class", "area below")
-                .attr("clip-path", "url(#clip-below)")
-                .attr("d", area);
-
-            me.svg.append("path")
-                .attr("class", "line")
-                .attr("d", line);
-
-            me.svg.append("g")
-                .attr("class", "x axis")
-                .attr("transform", "translate(0," + height + ")")
-                .call(xAxis);
-
-            me.svg.append("g")
-                .attr("class", "y axis")
-                .call(yAxis)
-                .append("text")
-                .attr("transform", "rotate(-90)")
-                .attr("y", 6)
-                .attr("dy", ".71em")
-                .style("text-anchor", "end")
-                .text("Temperature (ºF)"); 
-            }
-        // d3.tsv("data.tsv", function(error, data) {
-        //     if (error) throw error;
-
-        //     data.forEach(function(d) {
-        //         d[xName] = parseDate(d[xName]);
-        //         d[y1Name] = +d[y1Name];
-        //         d[y2Name] = +d[y2Name];
-        //     });
-
-        //     x.domain(d3.extent(data, function(d) {
-        //         return d[xName];
-        //     }));
-
-        //     y.domain([
-        //         d3.min(data, function(d) {
-        //             return Math.min(d[y1Name], d[y2Name]);
-        //         }),
-        //         d3.max(data, function(d) {
-        //             return Math.max(d[y1Name], d[y2Name]);
-        //         })
-        //     ]);
-
-        //     me.svg.datum(data);
-
-        //     me.svg.append("clipPath")
-        //         .attr("id", "clip-below")
-        //         .append("path")
-        //         .attr("d", area.y0(height));
-
-        //     me.svg.append("clipPath")
-        //         .attr("id", "clip-above")
-        //         .append("path")
-        //         .attr("d", area.y0(0));
-
-        //     me.svg.append("path")
-        //         .attr("class", "area above")
-        //         .attr("clip-path", "url(#clip-above)")
-        //         .attr("d", area.y0(function(d) {
-        //             return y(d[y2Name]);
-        //         }));
-
-        //     me.svg.append("path")
-        //         .attr("class", "area below")
-        //         .attr("clip-path", "url(#clip-below)")
-        //         .attr("d", area);
-
-        //     me.svg.append("path")
-        //         .attr("class", "line")
-        //         .attr("d", line);
-
-        //     me.svg.append("g")
-        //         .attr("class", "x axis")
-        //         .attr("transform", "translate(0," + height + ")")
-        //         .call(xAxis);
-
-        //     me.svg.append("g")
-        //         .attr("class", "y axis")
-        //         .call(yAxis)
-        //         .append("text")
-        //         .attr("transform", "rotate(-90)")
-        //         .attr("y", 6)
-        //         .attr("dy", ".71em")
-        //         .style("text-anchor", "end")
-        //         .text("Temperature (ºF)");
-        // });
-
+                    me.parentG.append("g")
+                        .attr("class", "y axis")
+                        .call(yAxis)
+                        .append("text")
+                        .attr("transform", "rotate(-90)")
+                        .attr("y", 6)
+                        .attr("dy", ".71em")
+                        .style("text-anchor", "end")
+                        .text("Temperature (ºF)"); 
+          */
     }
 
 });
