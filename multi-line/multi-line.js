634ff0b55ecc1f65b4d1d130e1c8111a3a21fb7f
aarunsoman <arunsoman@gmail.com>
2016-08-15 00:24:07 +0530

    multi line working

--- multi-line/multi-line.js
+++ multi-line/multi-line.js
@@ -11,12 +11,21 @@ Polymer({
                 selectedValue: -1,
                 uitype: 'single-value',
                 notify: true,
+                selectedObjs: [],
+                name:'',
+                scaleType: '',
+                format:'',
+
             }, {
                 input: 'y',
                 txt: 'Pick y',
                 selectedValue: [],
                 uitype: 'multi-value',
-                notify: true
+                notify: true,
+                selectedObjs: [],
+                name:'',
+                scaleType: '',
+                format:'',
             }]
         },
         settings: {
@@ -41,133 +50,83 @@ Polymer({
           me.source = data;
       }
       PolymerD3.fileReader("ms.csv", [1, 2, 3], [0], "%Y%m%d", callback);
+      this.inputs[0].selectedValue = 0;
+      this.inputs[0].name = 'time';
+      this.inputs[1].selectedValue  = [1,2,3];
+      this.inputs[1].name = ['New York','San Francisco', 'Austin'];
+
+      this.external = [ {'key':"Time", 'value':0, 'scaleType':'time', 'format':'time'},
+       {'key':"New York", 'value':1, 'scaleType':'linear', 'format':'number'},
+       {'key':"San Francisco", 'value':2, 'scaleType':'linear', 'format':'number'},
+       {'key':"Austin", 'value':3, 'scaleType':'linear', 'format':'number'}];
+
     },
 
     draw: function() {
       var me = this;
-
-      if (me.getInputsProperty('x') === -1 || me.getInputsProperty('y').length === 0) {
+      var xIndex = me.getInputsProperty('x');
+      var yIndex = me.getInputsProperty('y');
+      if ( xIndex === -1 || yIndex.length === 0) {
         throw new Error('Inputs not selected');
       }
+      var data = this.source;
+      var dataStat = PolymerD3.summarizeData(data, 0, 'time', [1,2,3], 'number', false);
+      var xBound = dataStat.getXDomain();
+      var yBound = dataStat.getYDomain();
 
-      me.makeChartWrap();
-      var margin = me.getMargins();
-      var width = me.getWidth() - margin.left - margin.right;
-      var height = me.getHeight() - margin.top - margin.bottom;
-      var svg = me.svg;
-      PolymerD3.setSvgArea (svg, width, height, margin);
-      var g = svg.select("g").attr("transform", "translate(" + margin.left + "," + margin.top + ")");   
-        
- 
-      //var parseTime = d3.timeParse("%Y%m%d");
-      var xAxis = PolymerD3.axis('time', 'Tabbrmonth');
-      var yAxis = PolymerD3.axis('number').orient("left");
-      var x = xAxis.scale().range([0, width]),
-          y = yAxis.scale().range([height, 0]),
-          color = d3.scale.category10();
+      var config = {'scaleType':"time", 
+        'align':'h', 'format':'time', 'position':'bottom','domain':xBound};
+      var xAxis = me.createAxis(config);
+      var x = xAxis.scale();
+
+      config = {'scaleType':"linear", 
+        'align':'v', 'format':'currency', 'position':'left','domain':[0, yBound[1]]};
+      var yAxis = me.createAxis(config);
+      var y = yAxis.scale();
+
+      var color = d3.scale.category10();
       //z = d3.scale.ordinal(d3.schemeCategory10);
+      var dataStruct = PolymerD3.rollupMultiValued(
+        this.inputs[1].selectedValue, 
+        this.inputs[1].name,
+        xIndex, data);
 
       var line = d3.svg.line().interpolate("basis")
-          .x(function(d) {
-              return x(d[0]);
-          })
-          .y(function(d) {
-              return y(d[1]);
+          .x( (d) => { 
+            return x(d.x);
+             })
+          .y( (d) => { 
+            return y(d.y); 
           });
 
-      var data = this.source;
-
-      //load this from input
-      var yHeaders = [];
-      me.getInputsProperty('y').forEach(function (ele) { 
-        me.external.forEach (function (anExternal) { 
-          if(anExternal.value === ele) { 
-            yHeaders.push([anExternal.key, anExternal.value]);
-          } 
-        });
-      });
-
-      //load this from input
-      var xInput = me.getInputsProperty('x'); 
-      var xHeader = [me.external[xInput].key, me.external[xInput].value];
-      
-      var cities = yHeaders.map(function(aRow) {
-          return {
-              id: aRow[0],
-              values: function() {
-                  var tArray = []
-                  data.map(function(d) {
-                      tArray.push([d[xHeader[1]], d[aRow[1]]]);
-                  });
-                  return tArray;
-              }()
-          };
-      });
-
-      x.domain(d3.extent(data, function(d) {
-          return d[xHeader[1]];
-      }));
-
-      y.domain([
-          d3.min(cities, function(c) {
-              return d3.min(c.values, function(d) {
-                  return d[1];
-              });
-          }),
-          d3.max(cities, function(c) {
-              return d3.max(c.values, function(d) {
-                  return d[1];
-              });
-          })
-      ]);
-
-      color.domain(cities.map(function(c) {
-          return c.id;
-      }));
-
-     g.append("g")
-          .attr("class", "axis axis--x")
-          .attr("transform", "translate(0," + height + ")")
-          .call(xAxis);
-
-     g.append("g")
-          .attr("class", "axis axis--y")
-          .call(yAxis)
-          .append("text")
-          .attr("transform", "rotate(-90)")
-          .attr("y", 6)
-          .attr("dy", "0.71em")
-          .attr("fill", "#000")
-          .text("Temperature, ºF");
-
-      var city = g.selectAll(".city")
-          .data(cities)
+      var aGroup = this.parentG.selectAll(".groups")
+          .data(dataStruct)
           .enter().append("g")
-          .attr("class", "city");
+          .attr("class", "groups");
 
-      city.append("path")
+      aGroup.append("path")
           .attr("class", "line")
-          .attr("d", function(d) {
-              return line(d.values);
+          .attr("d", (d) => { 
+            return line(d.values); 
           })
-          .style("stroke", function(d) {
-              return color(d.id);
+          .style("stroke", (d) => { 
+            return color(d.id); 
           });
 
-      city.append("text")
-          .datum(function(d) {
+      aGroup.append("text")
+          .datum((d) => {
               return {
                   id: d.id,
                   value: d.values[d.values.length - 1]
               };
           })
-          .attr("transform", function(d) {
-              return "translate(" + x(d.value[0]) + "," + y(d.value[1]) + ")";
+          .attr("transform", (d) => {
+              return "translate(" + x(d.value.x) + "," + y(d.value.y) + ")";
           })
           .attr("x", 3)
           .attr("dy", "0.35em")
           .style("font", "10px sans-serif")
-          .text(function(d) {
+          .text((d) => {
               return d.id;
           });
     }
