2bdcecce8e32b575489e32791708e3023b2b72c4
ess_ess <sravans2011@gmail.com>
2016-08-30 08:33:04 +0530

    Display component to show chart types created.

--- polymer-d3.js
+++ polymer-d3.js
@@ -8,27 +8,45 @@ Polymer({
       value: () => {
         return [{
           label: 'Stacked Bar Chart',
-          icon: 'icons:accessibility'
+          icon: 'icons:accessibility',
+          element: 'bar-chart',
+          callBack: 'setStackedSettings'
         }, {
           label: 'Grouped Bar Chart',
-          icon: 'icons:cloud-circle'
+          icon: 'icons:cloud-circle',
+          element: 'bar-chart',
+          callBack: 'setGroupedSettings'
         }, {
           label: 'Waterfall Chart',
-          icon: 'icons:accessibility'
+          icon: 'icons:accessibility',
+          element: 'bar-chart',
+          callBack: 'setWatefallSettings'
         }, {
           label: 'Pie Chart',
-          icon: 'icons:content-cut'
+          icon: 'icons:content-cut',
+          element: 'pie-chart',
+          callBack: 'setPieSettings'
         }, {
           label: 'Heat Map',
-          icon: 'icons:bug-report'
+          icon: 'icons:bug-report',
+          element: 'bar-chart',
+          callBack: 'setHeatMapSettings'
         }, {
           label: 'Area Chart',
-          icon: 'icons:dns'
+          icon: 'icons:dns',
+          element: 'area-chart',
+          callBack: 'setAreaSettings'
         }, {
           label: 'Sankey Chart',
-          icon: 'icons:check-circle'
-        },];
+          icon: 'icons:check-circle',
+          element: 'sankey-chart',
+          callBack: 'setSankeySettings'
+        }];
       }
+    },
+    selectedChart: {
+      type: Object,
+      value: () => { return {};}
     }
   },
 
