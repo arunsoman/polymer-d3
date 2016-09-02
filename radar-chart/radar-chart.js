ed82c053c4e3686ef52085d911849c189cf3deb2
aarunsoman <arunsoman@gmail.com>
2016-09-03 00:08:46 +0530

    radar now takes input config, however the load from multi col is pending

--- radar-chart/radar-chart.js
+++ radar-chart/radar-chart.js
@@ -6,15 +6,21 @@ Polymer({
       notify: true,
       type: Array,
       value: [{
-        input: 'slice',
+        input: 'x',
         txt: 'Pick a dimension',
         selectedValue: 0,
         selectedName: 'label',
         uitype: 'single-value'
       }, {
-        input: 'sliceSize',
+        input: 'y',
         txt: 'Pick a messure',
-        selectedValue: 1,
+        selectedValue: [1],
+        selectedName: 'count',
+        uitype: 'single-value'
+      },{
+        input: 'z',
+        txt: 'Pick a group',
+        selectedValue: [2],
         selectedName: 'count',
         uitype: 'single-value'
       }]
@@ -62,58 +68,61 @@ Polymer({
   _toggleView: function() {
     this.hideSettings = !this.hideSettings;
   },
-  
+
   attached: function() {
+    PolymerD3.fileReader.call(this,'radar-chart/data1.csv', [1], [], undefined, this.callme, true);
+
+  },
+  callme(d){
+    this.source = d;
+    let yIndices = this.getInputsProperty('y');
+    this.data = this.loadFromGroup(yIndices);
     this.draw();
+  },
+  loadFromMultiCol: function(yIndices){
+    let xIndex = this.getInputsProperty('x');
+  //  let zGroup = this.getInputsProperty('z');
+
+    var headers = this.source[0];
+    var data = [];
+    for(var i = 1; i < this.source.length; i++){
+      var data1 = [];
+      for(var j = 0; j <yIndices.length; j++){
+        var tA= [];
+        tA.push(headers[yIndices[j]]);
+        tA.push(this.source[i][j]);
+        tA.push(this.source[i][xIndex]);
+        data1.push(tA);
+      }
+      data.push(data1);
+    }
+    return data;
+  },
+  loadFromGroup: function(yIndices){
+    let xIndex = this.getInputsProperty('x');
+    //let yIndices = this.getInputsProperty('y');
+    let zGroup = this.getInputsProperty('z');
+    this.source.shift();
+    var layers = d3.nest()
+    .key((d)=>{console.log(d[zGroup]);return d[zGroup];})
+    .entries(this.source);
+    var data  = [];
+    layers.forEach((element)=>{
+      var data1 = [];
+      element.values.forEach((value)=>{
+        data1.push([value[xIndex],value[yIndices[0]], value[zGroup]]);
+      });
+      data.push(data1);
+    });
+    return data;
   },
 
   draw: function() {
-
-var margin = {top: 100, right: 100, bottom: 100, left: 100},
-        width = Math.min(700, window.innerWidth - 10) - margin.left - margin.right,
-        height = Math.min(width, window.innerHeight - margin.top - margin.bottom - 20);
-          
-      ////////////////////////////////////////////////////////////// 
-      ////////////////////////// Data ////////////////////////////// 
-      ////////////////////////////////////////////////////////////// 
-
-      var data = [
-            [//iPhone
-            ["Battery Life",0.22],
-            ["Brand",0.28],
-            ["Contract Cost",0.29],
-            ["Design And Quality",0.17],
-            ["Have Internet Connectivity",0.22],
-            ["Large Screen",0.02],
-            ["Price Of Device",0.21],
-            ["To Be A Smartphone",0.50]      
-            ],[//Samsung
-            ["Battery Life",0.27],
-            ["Brand",0.16],
-            ["Contract Cost",0.35],
-            ["Design And Quality",0.13],
-            ["Have Internet Connectivity",0.20],
-            ["Large Screen",0.13],
-            ["Price Of Device",0.35],
-            ["To Be A Smartphone",0.38]
-            ],[//Nokia Smartphone
-            ["Battery Life",0.26],
-            ["Brand",0.10],
-            ["Contract Cost",0.30],
-            ["Design And Quality",0.14],
-            ["Have Internet Connectivity",0.22],
-            ["Large Screen",0.04],
-            ["Price Of Device",0.41],
-            ["To Be A Smartphone",0.30]
-            ]
-          ];
-      ////////////////////////////////////////////////////////////// 
-      //////////////////// Draw the Chart ////////////////////////// 
-      ////////////////////////////////////////////////////////////// 
-
+      if(this.data === undefined)
+        return;
       var color = d3.scale.ordinal()
         .range(["#EDC951","#CC333F","#00A0B0"]);
-        
+
       var radarChartOptions = {
         w: this.chartWidth,
         h: this.chartHeight,
@@ -124,6 +133,6 @@ var margin = {top: 100, right: 100, bottom: 100, left: 100},
         color: color
       };
       //Call function to draw the Radar chart
-      PolymerD3.RadarChart(".radarChart", data, radarChartOptions, this);
+      PolymerD3.RadarChart( this.data, radarChartOptions, this);
   }
 });
