151d1294f3a391d023c207568bc83abfcc3347de
prathap937 <prathap937@gmail.com>
2017-07-12 17:34:22 +0530

    updated pie chart logic,adde legend color change picker

---
+++ display-component/data.js
@@ -0,0 +1,23 @@
+var datacsv={
+  externals:[{
+    key: 'Items',
+    type: 'String',
+    value: 0
+  }, {
+    key: 'Percentage',
+    type: 'Number',
+    value: 1
+  }, {
+    key: 'Q3',
+    type: 'Number',
+    value: 2
+  }, {
+    key: 'Q4',
+    type: 'Number',
+    value: 3
+  }],
+  source:[
+  ["Java",50,0,0],
+  ["PHP",100,0,0]
+  ]
+}
