d6d4790ed206963876356bfb834c03bc47ef174f
Arun Kumar A T <arunkumar.thampi@flytxt.com>
2017-07-14 13:01:24 +0530

    old files are removed

--- pie-chart/pie-chart-n.js
+++
@@ -1,7 +0,0 @@
-class pieChart extends Polymer.mixinBehaviors([],ReduxMixin(chartMixin(Polymer.Element))){
-  static get is(){return"pie-chart"}
-
-
-
-}
-customElements.define(pieChart.is, pieChart)
